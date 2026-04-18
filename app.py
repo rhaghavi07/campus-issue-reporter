2a15a902b746704f2652f6b3adbcda3fcbe08af2
rhaghavi07 <rhaghavi02@gmail.com>
2026-04-18 22:48:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -219,5 +219,7 @@ def dashboard():
 
 # ---------- RUN ----------
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
